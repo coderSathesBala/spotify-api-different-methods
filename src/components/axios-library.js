31ebcdd11a5a2c61db329583d0f91348055a6386
coderSathesBala <sathes352@gmail.com>
2022-01-07 10:08:10 -0500

    CleaningCode

--- src/components/axios-library.js
+++ src/components/axios-library.js
@@ -45,14 +45,14 @@ const PersonList = () => {
     }
          return(
              <div>
-                 <button onClick={handleLogIn}
-                 >Sign-In With Spotify</button>
-                 <h1>DISPLAY ARTIST NAME:</h1>
-          <h1>DISPLAY ARTIST PICTURE:</h1>
-          <h1>DISPLAY ARTIST ALBUM:</h1>
-          <h1>DISPLAY ALBUM PICTURE:</h1>
-          <h1>DISPLAY ALBUM TRACKLIST:</h1>
-          <h1>DISPLAY ALBUM INFO:</h1>
+                <button onClick={handleLogIn}
+                >Sign-In With Spotify</button>
+                <h1>DISPLAY ARTIST NAME:</h1>
+                <h1>DISPLAY ARTIST PICTURE:</h1>
+                <h1>DISPLAY ARTIST ALBUM:</h1>
+                <h1>DISPLAY ALBUM PICTURE:</h1>
+                <h1>DISPLAY ALBUM TRACKLIST:</h1>
+                <h1>DISPLAY ALBUM INFO:</h1>
              </div>
          )
 }
